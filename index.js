3aa5fdbdfc750b8b5dcc7d7ef5ab1a1ee6397911
Mihaela Constantin <mihacon67@gmail.com>
2022-07-25 20:16:12 +0300

    CV 2

---
+++ index.js
@@ -0,0 +1,13 @@
+// var activePage = "home";
+
+// function showpage(id) {
+//   hideAllPages();
+//   showpage(id);
+//   console.warn("preview page", activePage);
+//   document.getElementById("menu-" + activePage).classList.remove("active");
+//   document.getElementById("menu-" + id).classList.add("active");
+//   activPage = id;
+// }
+
+// // showpage("languages");
+// showpage(activePage);
