329fc84bfe369916afc0ea899e8ce518c598c2ee
Mihaela Constantin <mihacon67@gmail.com>
2022-10-14 14:09:18 +0300

    Added var in js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-console.info(2);
 console.info("page loaded");
-console.info("ce faci?");
 console.debug(23 + 2);
 console.warn(6 * 3 - 10);
-console.error("app started");
+console.error("started");
+
+var myName = "Mihaela";
+var age = 19;
+
+console.info("My name is " + myName + ",I'm " + age + " years old.");
